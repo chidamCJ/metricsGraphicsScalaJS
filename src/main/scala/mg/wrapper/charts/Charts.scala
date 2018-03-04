66e913131a182f88344f1e6543671f28e28e448f
Prasanna Kumar <prassee@prassee.me>
2018-03-04 20:18:20 +0530

    updated UI and case classes

--- src/main/scala/mg/wrapper/charts/Charts.scala
+++ src/main/scala/mg/wrapper/charts/Charts.scala
@@ -1,14 +1,37 @@
 package mg.wrapper.charts
+
+import org.scalajs.dom.document
+
 import scala.scalajs.js
-import js.JSConverters._
-case class ChartParams(title: String)
+import scala.scalajs.js.JSConverters._
+
+case class ChartParams(title: String, id: String, dataSet: Either[SingleDataSet, MultiDataSet]) {
+
+  def populate = {
+    println(s"plotting ${title} ${id} ${dataSet.isLeft} ${dataSet.isRight}")
+    js.Dynamic.literal(
+      "title"      -> title,
+      "area"       -> true,
+      "data"       -> (if (dataSet.isLeft) dataSet.left.get.asJs else dataSet.right.get.asJs),
+      "width"      -> 600,
+      "height"     -> 240,
+      "right"      -> 40,
+      "left"       -> 90,
+      "bottom"     -> 50,
+      "target"     -> document.getElementById(id),
+      "x_accessor" -> "over",
+      "y_accessor" -> "score"
+    )
+  }
+}
 case class DataColumn(key: String, value: Double)
 case class DataRow(cols: Array[DataColumn]) {
   def asDict = js.Dictionary(cols.map(x => (x.key, x.value)): _*)
 }
-case class DataSet(rows: Array[DataRow]) {
+
+case class SingleDataSet(rows: Array[DataRow]) {
   def asJs = rows.map(_.asDict).toJSArray
 }
-case class MultiDataSet(rowsArray: Array[DataSet]) {
+case class MultiDataSet(rowsArray: Array[SingleDataSet]) {
   def asJs = rowsArray.map(_.asJs).toJSArray
 }
--- src/main/scala/mg/wrapper/mg/wrapper/ui/MetricsGraphicsUI.scala
+++ src/main/scala/mg/wrapper/mg/wrapper/ui/MetricsGraphicsUI.scala
@@ -2,13 +2,78 @@ package mg.wrapper.mg.wrapper.ui
 
 import mg.wrapper.MGWrapper
 import mg.wrapper.charts._
+import org.scalajs.dom.document
 
+import scala.scalajs.js.annotation.{ JSExport, JSExportTopLevel }
+
+@JSExportTopLevel("mgui")
 object MetricsGraphicsUI {
 
-  def plot() {
-    MGWrapper.plot(ChartParams("scores-level-chart"))
+  private val data = SingleDataSet(
+    Array(
+      DataRow(Array(DataColumn("over", 1), DataColumn("score", 12))),
+      DataRow(Array(DataColumn("over", 2), DataColumn("score", 18))),
+      DataRow(Array(DataColumn("over", 3), DataColumn("score", 2))),
+      DataRow(Array(DataColumn("over", 4), DataColumn("score", 8))),
+      DataRow(Array(DataColumn("over", 5), DataColumn("score", 3))),
+      DataRow(Array(DataColumn("over", 6), DataColumn("score", 10))),
+      DataRow(Array(DataColumn("over", 7), DataColumn("score", 12))),
+      DataRow(Array(DataColumn("over", 8), DataColumn("score", 15))),
+      DataRow(Array(DataColumn("over", 9), DataColumn("score", 16))),
+      DataRow(Array(DataColumn("over", 10), DataColumn("score", 18))),
+      DataRow(Array(DataColumn("over", 11), DataColumn("score", 18))),
+      DataRow(Array(DataColumn("over", 12), DataColumn("score", 6))),
+      DataRow(Array(DataColumn("over", 13), DataColumn("score", 12))),
+      DataRow(Array(DataColumn("over", 14), DataColumn("score", 18)))
+    )
+  )
+
+  private val multiData = MultiDataSet(
+    Array(
+      SingleDataSet(
+        Array(
+          DataRow(Array(DataColumn("over", 1), DataColumn("score", 12))),
+          DataRow(Array(DataColumn("over", 2), DataColumn("score", 18))),
+          DataRow(Array(DataColumn("over", 3), DataColumn("score", 2))),
+          DataRow(Array(DataColumn("over", 4), DataColumn("score", 8)))
+        )
+      ),
+      SingleDataSet(
+        Array(
+          DataRow(Array(DataColumn("over", 1), DataColumn("score", 3))),
+          DataRow(Array(DataColumn("over", 2), DataColumn("score", 10))),
+          DataRow(Array(DataColumn("over", 3), DataColumn("score", 12))),
+          DataRow(Array(DataColumn("over", 4), DataColumn("score", 15)))
+        )
+      ),
+      SingleDataSet(
+        Array(
+          DataRow(Array(DataColumn("over", 1), DataColumn("score", 33))),
+          DataRow(Array(DataColumn("over", 2), DataColumn("score", 13))),
+          DataRow(Array(DataColumn("over", 3), DataColumn("score", 21))),
+          DataRow(Array(DataColumn("over", 4), DataColumn("score", 54)))
+        )
+      )
+    )
+  )
+
+  @JSExport
+  def drawSingleGraph() = {
+    document.body.appendChild(chartElem("chart"))
+    MGWrapper.plot(ChartParams("single chart data", "chart", Left(data)))
   }
 
-  def main(args: Array[String]): Unit =
-    plot()
+  @JSExport
+  def drawMultiGraph() = {
+    document.body.appendChild(chartElem("multichart"))
+    MGWrapper.plot(ChartParams("multi chart data", "multichart", Right(multiData)))
+  }
+
+  private def chartElem(id: String) = {
+    val divElem = document.createElement("div")
+    divElem.setAttribute("id", id)
+    divElem
+  }
+
+  def main(args: Array[String]): Unit = {}
 }
