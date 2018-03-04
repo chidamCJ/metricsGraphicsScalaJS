c13edc69c38164235e6dfe9dcab4cc546af84694
Prasanna Kumar <prassee@prassee.me>
2018-03-04 17:28:19 +0530

    addmore example

--- src/main/scala/mg/wrapper/MGWrapper.scala
+++ src/main/scala/mg/wrapper/MGWrapper.scala
@@ -6,23 +6,91 @@ import scala.scalajs.js
 
 object MGWrapper {
 
-  def write(chartParams: ChartParams) = {
-    val data = js.Array(
-      js.Dictionary("over" -> "1", "score" -> "12"),
-      js.Dictionary("over" -> "2", "score" -> "18"),
-      js.Dictionary("over" -> "3", "score" -> "12"),
-      js.Dictionary("over" -> "4", "score" -> "18")
+  def plot(chartParams: ChartParams) = {
+    val data = DataSet(
+      Array(
+        DataRow(Array(DataColumn("over", 1), DataColumn("score", 12))),
+        DataRow(Array(DataColumn("over", 2), DataColumn("score", 18))),
+        DataRow(Array(DataColumn("over", 3), DataColumn("score", 2))),
+        DataRow(Array(DataColumn("over", 4), DataColumn("score", 8))),
+        DataRow(Array(DataColumn("over", 5), DataColumn("score", 3))),
+        DataRow(Array(DataColumn("over", 6), DataColumn("score", 10))),
+        DataRow(Array(DataColumn("over", 7), DataColumn("score", 12))),
+        DataRow(Array(DataColumn("over", 8), DataColumn("score", 15))),
+        DataRow(Array(DataColumn("over", 9), DataColumn("score", 16))),
+        DataRow(Array(DataColumn("over", 10), DataColumn("score", 18))),
+        DataRow(Array(DataColumn("over", 11), DataColumn("score", 18))),
+        DataRow(Array(DataColumn("over", 12), DataColumn("score", 6))),
+        DataRow(Array(DataColumn("over", 13), DataColumn("score", 12))),
+        DataRow(Array(DataColumn("over", 14), DataColumn("score", 18)))
+      )
     )
+    val multiData = MultiDataSet(
+      Array(
+        DataSet(
+          Array(
+            DataRow(Array(DataColumn("over", 1), DataColumn("score", 12))),
+            DataRow(Array(DataColumn("over", 2), DataColumn("score", 18))),
+            DataRow(Array(DataColumn("over", 3), DataColumn("score", 2))),
+            DataRow(Array(DataColumn("over", 4), DataColumn("score", 8)))
+          )
+        ),
+        DataSet(
+          Array(
+            DataRow(Array(DataColumn("over", 1), DataColumn("score", 3))),
+            DataRow(Array(DataColumn("over", 2), DataColumn("score", 10))),
+            DataRow(Array(DataColumn("over", 3), DataColumn("score", 12))),
+            DataRow(Array(DataColumn("over", 4), DataColumn("score", 15)))
+          )
+        ),
+        DataSet(
+          Array(
+            DataRow(Array(DataColumn("over", 1), DataColumn("score", 33))),
+            DataRow(Array(DataColumn("over", 2), DataColumn("score", 13))),
+            DataRow(Array(DataColumn("over", 3), DataColumn("score", 21))),
+            DataRow(Array(DataColumn("over", 4), DataColumn("score", 54)))
+          )
+        )
+      )
+    ).asJs
+
+    val multichart = document.createElement("div")
+    multichart.setAttribute("id", "multichart")
+    document.body.appendChild(multichart)
+
     MG(
       js.Dynamic.literal(
-        "title"      -> chartParams.title,
-        "area"       -> true,
-        "data"       -> data,
-        "width"      -> 650,
-        "height"     -> 200,
-        "target"     -> document.getElementById("chart"),
-        "x_accessor" -> "over",
-        "y_accessor" -> "score"
+        "title"            -> chartParams.title,
+        "area"             -> true,
+        "data"             -> data.asJs,
+        "animate_on_load"  -> true,
+        "width"            -> 600,
+        "height"           -> 240,
+        "right"            -> 40,
+        "left"             -> 90,
+        "bottom"           -> 50,
+        "target"           -> document.getElementById("chart"),
+        "x_accessor"       -> "over",
+        "x_extended_ticks" -> true,
+        "y_accessor"       -> "score"
+      )
+    )
+
+    MG(
+      js.Dynamic.literal(
+        "title"            -> chartParams.title,
+        "area"             -> true,
+        "data"             -> multiData,
+        "animate_on_load"  -> true,
+        "width"            -> 600,
+        "height"           -> 240,
+        "right"            -> 40,
+        "left"             -> 90,
+        "bottom"           -> 50,
+        "target"           -> document.getElementById("multichart"),
+        "x_accessor"       -> "over",
+        "x_extended_ticks" -> true,
+        "y_accessor"       -> "score"
       )
     )
   }
--- src/main/scala/mg/wrapper/charts/Charts.scala
+++ src/main/scala/mg/wrapper/charts/Charts.scala
@@ -1,3 +1,14 @@
 package mg.wrapper.charts
-
+import scala.scalajs.js
+import js.JSConverters._
 case class ChartParams(title: String)
+case class DataColumn(key: String, value: Double)
+case class DataRow(cols: Array[DataColumn]) {
+  def asDict = js.Dictionary(cols.map(x => (x.key, x.value)): _*)
+}
+case class DataSet(rows: Array[DataRow]) {
+  def asJs = rows.map(_.asDict).toJSArray
+}
+case class MultiDataSet(rowsArray: Array[DataSet]) {
+  def asJs = rowsArray.map(_.asJs).toJSArray
+}
