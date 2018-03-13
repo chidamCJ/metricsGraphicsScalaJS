7718a25919bb8d0ada4b3ad624dfc4ae914ff807
chidambara raja <chidam.sky@gmail.com>
2018-03-13 17:47:13 +0530

    changes made for case classes with trait

--- mgwrapper/src/main/scala/mg/wrapper/charts/MGChartAttribs.scala
+++ mgwrapper/src/main/scala/mg/wrapper/charts/MGChartAttribs.scala
@@ -1,5 +1,94 @@
 package mg.wrapper.charts
 
-case class MGChartAttribs (title: String,description: String,width: Int,height: Int,right: Int,target: String,x_accessor: String,y_accessor: String)
-case class LineChart()extends MGChartAttribs
-case class ConfidenceBand(format:String,area: Boolean,show_secondary_x_label:Boolean,show_confidence_band: Array[String], x_extended_ticks:Boolean)extends MGChartAttribs
+sealed trait BaseChartAttributes {
+  def getAttributes: Array[(String, Any)]
+}
+
+case class MGChartAttribs(title: String,
+                          description: String,
+                          width: Int,
+                          height: Int,
+                          right: Int,
+                          target: String,
+                          x_accessor: String,
+                          y_accessor: String)
+    extends BaseChartAttributes {
+  override def getAttributes: Array[(String, Any)] =
+    Array(
+      "title" -> title,
+      "description" -> description,
+      "width" -> width,
+      "height" -> height,
+      "right" -> right,
+      "target" -> target,
+      "x_accessor" -> x_accessor,
+      "y_accessor" -> y_accessor
+    )
+}
+case class LineChart(title: String,
+                     description: String,
+                     width: Int,
+                     height: Int,
+                     right: Int,
+                     target: String,
+                     x_accessor: String,
+                     y_accessor: String)
+    extends BaseChartAttributes {
+  override def getAttributes: Array[(String, Any)] =
+    Array(
+      "title" -> title,
+      "description" -> description,
+      "width" -> width,
+      "height" -> height,
+      "right" -> right,
+      "target" -> target,
+      "x_accessor" -> x_accessor,
+      "y_accessor" -> y_accessor
+    )
+}
+case class ConfidenceBand(title: String,
+                          description: String,
+                          format: String,
+                          width: Int,
+                          height: Int,
+                          right: Int,
+                          area: Boolean,
+                          target: String,
+                          show_secondary_x_label: Boolean,
+                          show_confidence_band: Array[String],
+                          x_extended_ticks: Boolean)
+    extends BaseChartAttributes {
+  override def getAttributes: Array[(String, Any)] =
+    Array(
+      "title" -> title,
+      "description" -> description,
+      "format" -> format,
+      "width" -> width,
+      "height" -> height,
+      "right" -> right,
+      "area" -> area,
+      "target" -> target,
+      "show_secondary_x_label" -> show_secondary_x_label,
+      "show_confidence_band" -> show_confidence_band,
+      "x_extended_ticks" -> x_extended_ticks
+    )
+}
+case class SmallRangeOfIntegers(title: String,
+                                description: String,
+                                interpolate: Any,
+                                width: Int,
+                                height: Int,
+                                right: Int,
+                                target: String)
+    extends BaseChartAttributes {
+  override def getAttributes: Array[(String, Any)] =
+    Array(
+      "title" -> title,
+      "description" -> description,
+      "interpolate" -> interpolate,
+      "width" -> width,
+      "height" -> height,
+      "right" -> right,
+      "target" -> target,
+    )
+}
